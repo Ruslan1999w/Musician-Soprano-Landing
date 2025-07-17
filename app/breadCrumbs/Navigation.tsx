f088672254c252829c784a33ddbb08e23b964ae2
Ruslan <plaksunovrr@gnivc.ru>
2025-07-17 19:49:54 +0300

    added mobile navigation and fixed header on mobiles

--- app/breadCrumbs/Navigation.tsx
+++ app/breadCrumbs/Navigation.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 
 export const Navigation = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [openNavMobile, setOpenNavMobile] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -22,19 +23,49 @@ export const Navigation = () => {
         };
     }, []);
 
+    const toggleNavMobile = () => setOpenNavMobile(!openNavMobile);
+
+    const navMobile = openNavMobile && (
+        <section className='fixed z-[1000] top-0 bottom-0 left-0 right-0 h-full w-full bg-black p-0'>
+            <div className='h-full flex flex-col relative m-0 ml-auto'>
+                <button className='w-6 h-6 block border-none text-white fixed z-10 top-7 right-7 cursor-pointer'
+                        onClick={() => setOpenNavMobile(false)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                        <path
+                            d="m0 88.591 38.926-38.927L0 10.738 11.409 0l38.927 38.926L88.591 0 100 10.738 61.074 49.664 100
+                            88.591 88.591 99.329 49.664 61.074 10.738 100z"
+                            fill="currentColor"
+                            fill-rule="nonzero"
+                        />
+                    </svg>
+                </button>
+                <div className='w-full m-auto overflow-auto'>
+                    <ul className='flex-col flex gap-8 overflow-hidden justify-between m-0' onClick={toggleNavMobile}>
+                        <li className='text-center mt-4 mb-4 ml-0 mr-0'>
+                            <Link href="/#article" className="hover:text-gray-300 transition">About</Link>
+                        </li>
+                        <li className='text-center mt-4 mb-4 ml-0 mr-0'>
+                            <Link href="/repertoire" className="hover:text-gray-300 transition" scroll={false}>Repertoire</Link>
+                        </li>
+                        <li className='text-center mt-4 mb-4 ml-0 mr-0'>
+                            <Link href="/media" className="hover:text-gray-300 transition" scroll={false}>Media</Link>
+                        </li>
+                        <li className='text-center mt-4 mb-4 ml-0 mr-0'>
+                            <Link href="/contact" className="hover:text-gray-300 transition" scroll={false}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+
     return (
         <header className={`
-        fixed
-        w-full
-        z-50 bg-transparent text-white pt-[2.5rem] px-[3rem]
-        h-auto
-        pb-[3rem]
-        ml-auto
-        mr-auto
-        transition-all duration-150 ease-in-out
-        ${scrolled ? `${styles.scrolled} bg-white/50 `: `${styles.not_scrolled}`}
+        fixed w-full z-50 bg-transparent text-white pt-[2.5rem] px-[3rem] h-auto pb-[3rem] ml-auto mr-auto transition-all duration-150 ease-in-out
+        ${scrolled ? `${styles.scrolled} bg-white/50 ` : `${styles.not_scrolled}`}
         `}>
-            <div className={`container mx-auto flex justify-between items-center max-w-7xl w-full h-[60px]`}>
+            <div className={`container mx-auto flex justify-between items-center max-w-7xl w-full md:h-[60px]`}>
                 <div className={styles.logo}>
                     <Link href="/">Kristina Sokova</Link>
                 </div>
@@ -47,26 +78,19 @@ export const Navigation = () => {
                     <Link href="/contact" className="hover:text-gray-300 transition" scroll={false}>Contact</Link>
                 </nav>
 
-
                 {/* Mobile Menu Button */}
                 <button
                     className="md:hidden"
+                    onClick={() => {setOpenNavMobile(true)}}
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M4 6h16M4 12h16M4 18h16"
-                        />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 25" className="w-[30px] h-[19px] text-white">
+                        <g fill="currentColor">
+                            <path d="M0 0h35v3H0zM0 11h35v3H0zM0 22h35v3H0z"/>
+                        </g>
                     </svg>
                 </button>
+
+                {navMobile}
             </div>
         </header>
     )
